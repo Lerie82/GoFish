8b80524e78938e44f49917ff407e9cec953de6f0
lerie82 <ltaylor82619@gmail.com>
2020-09-02 23:43:31 -0700

    &nbsp;

--- wp.sh
+++ wp.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 # Lerie Taylor
+
+#find some web servers. maybe.
+#nmap -vv --open -p80 -iR 100 -oG - >>tmp/wp
+
 sites=`cat tmp/wp|tr ":" "-"`
 iplist=`echo $sites|grep -Eo '[[:digit:]]{1,3}\.[[:digit:]]{1,3}\.[[:digit:]]{1,3}\.[[:digit:]]{1,3}'`
 
 for ip in $iplist
 do
+	echo $ip
 	#dig it up
 	#dig -x $ip ANY +short
 
 	#check for wp
-	curl -v "https://$ip/wp-json/"
+	#curl -o "wp/$ip" -v "https://$ip/wp-json/"
 done
