d72fffdf05d40414b7e95be7ec843b1d8af213aa
lerie82 <ltaylor82619@gmail.com>
2020-09-02 22:04:49 -0700

    still pushing..

--- pscan.sh
+++ pscan.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
 #Lerie Taylor / 09-02-20
 
-#count=`wc -l tmp/proxylist |cut -d' ' -f1`
-#r=$((RANDOM%${count}));
-#host=`cat tmp/proxylist|head -n$r|tail -n1`
-#info=( $(echo $host|tr ":" " ") )
-#export http_proxy=http://${info[0]}:${info[1]}
-#export https_proxy=$http_proxy
-#echo "proxy: ${info[0]}:${info[1]}"
-
 cat tmp/proxylist|sort -u >tmp/plist
 mv tmp/plist tmp/proxylist
 
@@ -18,24 +10,27 @@ for proxy in ${plist}
 do
 	info=( $(echo $proxy|tr ":" " ") )
 
-	echo "setting HTTP proxy"
+	echo "setting HTTP/S proxy"
 	export http_proxy=http://${info[0]}:${info[1]}
 	export https_proxy=$http_proxy
 
-	echo "digging up bones"
-	dig -x ${info[0]} ANY +short >>tmp/digger
+	#echo "digging up bones"
+	#dig -x ${info[0]} ANY +short >>tmp/digger
 
-	echo "digging up mail recs"
-	dig -x ${info[0]} MX +short >>tmp/mail
+	#echo "digging up mail recs"
+	#dig -x ${info[0]} MX +short >>tmp/mail
 
-	echo "Testing: $info"
-	result=$(curl -sb -m10 https://api.ipify.org/)
+	echo "Testing proxy: $info"
+	result=$(curl -s -m10 https://api.ipify.org/)
 
 	if [ "$result" = "${info[0]}" ];
 	then
 		echo "${info[0]}:${info[1]}" >>tmp/saved
 	fi
 done
+
+#download the proxylist from server
+curl https://www.lerietaylor.com/proxylist >>tmp/saved
 
 echo "removing dupes"
 #remove duplicates
--- wp.sh
+++ wp.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
 # Lerie Taylor
+sites=`cat tmp/wp|tr ":" "-"`
+iplist=`echo $sites|grep -Eo '[[:digit:]]{1,3}\.[[:digit:]]{1,3}\.[[:digit:]]{1,3}\.[[:digit:]]{1,3}'`
+
+for ip in $iplist
+do
+	#dig it up
+	#dig -x $ip ANY +short
+
+	#check for wp
+	curl -v "https://$ip/wp-json/"
+done
